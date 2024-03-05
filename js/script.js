0e22bc0d1424557285b7e969aac372e7d4c05df3
s4r44d43 <sararez@live.it>
2024-03-05 18:53:00 +0100

    [generate] random bombs

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 
 const bigSq = document.querySelector('.big_square');
+const btnStart = document.querySelector('.start');
 const btnReset = document.querySelector('.reset');
 
-for( let i = 1; i <= 100; i++){
-  const insertSq = functionInsertSq (i);
-  bigSq.append(insertSq);
-};
+let bombs = [];
+const maxBombs = 16;
 
+let score = 0;
+
+
+btnStart.addEventListener('click', function(){
+
+  for( let i = 1; i <= 100; i++){
+    const insertSq = functionInsertSq (i);
+    bigSq.append(insertSq);
+  };
+
+  
+});
 
 btnReset.addEventListener('click', function(){
   reset ();
@@ -20,16 +31,56 @@ function functionInsertSq (numero){
 
   const sq = document.createElement('div');
   sq.className = 'small_square';
-  
 
   sq.addEventListener('click', function(){
 
     this.innerHTML = numero;
     this.classList.add('choosed');
+
+    bombs = createBomb();
+
+    if(bombs.includes(numero)){
+
+      endGame (' hai perso ');
+      this.classList.add('.bomb');
+
+    } else {
+
+      this.innerHTML = numero;
+      this.classList.add('choosed');
+      score++;
+      if(score === 100 - maxBombs){
+        endGame ('hai vinto');
+      };
+
+    };
+
+
+    console.log(bombs);
     
   });
 
   return sq;
+};
+
+function createBomb(){
+
+  const bombsCase = [];
+
+  while (bombsCase.length < maxBombs){
+
+    const singleBomb = Math.ceil(Math.random() * 100);
+
+    if (!bombsCase.includes(singleBomb)) bombsCase.push(singleBomb);
+
+  };
+
+  return bombsCase;
+
+};
+
+function endGame(winLose){
+  console.log(winLose);
 };
 
 function reset (){
